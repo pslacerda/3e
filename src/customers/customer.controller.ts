2b996912881af143acbf4205fa344c610b422372
Pedro Lacerda <pslacerda@gmail.com>
2021-04-13 04:25:19 -0300

    OpenAPI (Swagger)

--- src/customers/customer.controller.ts
+++ src/customers/customer.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { CustomerService } from './customer.service';
 
-@Controller()
+@Controller('customers')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: CustomerService) {}
 
   @Get()
   getHello(): string {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe);
+
+  SwaggerModule.setup('api', app,
+    SwaggerModule.createDocument(app,
+      new DocumentBuilder()
+        .setTitle('Customers API')
+        .setDescription('My customers API')
+        .setVersion('1.0')
+        .addTag('customers')
+        .build()
+    )
+  );
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
